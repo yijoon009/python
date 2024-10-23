6cd727547c632da0321b55b8a1c535ad9a803bf3
yijoon009 <yijoon009@gmail.com>
2024-10-23 23:44:21 +0900

    1012-2

--- algorithm/1023/str_overwrite.py
+++ algorithm/1023/str_overwrite.py
@@ -1,5 +1,4 @@
 def solution(my_string, overwrite_string, s):
     before_str = my_string[:s]
-    after_str = my_string[s+len(overwrite_string):-1]
-    print(before_str+overwrite_string+after_str)
+    after_str = my_string[s+len(overwrite_string):]
     return before_str+overwrite_string+after_str
